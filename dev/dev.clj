4c2846c51f35deff30f52bac8d2ae6b8577f3b5d
Matt Burbidge <burbma@gmail.com>
2022-04-05 12:49:54 -0600

    add dev/dev.clj

---
+++ dev/dev.clj
@@ -0,0 +1,6 @@
+(ns dev
+  (:require [com.oncurrent.zeno.crdt :as crdt]
+            [com.oncurrent.zeno.crdt.commands :as commands]
+            [com.oncurrent.zeno.utils :as u]
+            [deercreeklabs.lancaster :as l]
+            [taoensso.timbre :as log]))
