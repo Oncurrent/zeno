6eeaf3ad6cba3332994086186ce390499f974193
Chad Harrington <chad.harrington@gmail.com>
2022-07-06 13:42:04 -0600

    Update CRDT op layout for "Option Red".

--- src/com/oncurrent/zeno/state_providers/crdt/server_crdt.clj
+++ src/com/oncurrent/zeno/state_providers/crdt/server_crdt.clj
@@ -1,5 +1,6 @@
 (ns com.oncurrent.zeno.state-providers.crdt.server-crdt
   (:require
+   [com.oncurrent.zeno.state-providers.crdt.common :as c]
    [com.oncurrent.zeno.utils :as u]
    [deercreeklabs.lancaster :as l]
    [deercreeklabs.lancaster.fingerprint :as lfp]
@@ -19,16 +20,21 @@
 ;;
 ;; 0                   1                   2                   3
 ;; 0 1 2 3 4 5 6 7 8 9 0 1 2 3 4 5 6 7 8 9 0 1 2 3 4 5 6 7 8 9 0 1
-;; +---------------------------------------------------------------+
-;; |                                                               |
-;; | Path                                                          |
-;; | Header                                                        |
-;; | (32)                                                          |
-;; +---------------------------------------------------------------+
+;; +-+-+-+-+-------+----------------+------------------------------+
+;; |V|O|V|P|       |                |                              |
+;; |E|T|T|L| Path  | System time    | Add                          |
+;; |R|Y|Y|E| Header| Milliseconds   | ID                           |
+;; | |P|P|N| (4)   | (8)            | (16)                         |
+;; +-+-+-+-+-------+----------------+------------------------------+
 ;; |                                                               |
 ;; | Path
 ;; |                                                               |
-;; | (160)
+;; | (192)
+;; +- - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -+
+;; |                                                               |
+;;   Path
+;; | (cont'd)                                                      |
+;;
 ;; +- - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -+
 ;; |                                                               |
 ;;   Path
@@ -49,12 +55,7 @@
 ;;   Path                                                          |
 ;; | (cont'd)                                                      |
 ;;                                                                 |
-;; +-+-+-+---------+----------------+------------------------------+
-;; |V|O|V| Reserved| System         |                              |
-;; |E|T|T| Padding | Time           | Add                          |
-;; |R|Y|Y|         | Milliseconds   | ID                           |
-;; | |P|P| (5)     | (8)            | (16)                         |
-;; +-+-+-+-+-------+----------------+------------------------------+
+;; +---------------------------------------------------------------+
 ;; |                                                               |
 ;; | Value                                                         |
 ;; |                                                               |
@@ -64,6 +65,7 @@
 ;;  VER = Version
 ;;  OTYP = Op Type
 ;;  VTYP = Value Type
+;;  PLEN = Path Length
 ;;
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 
@@ -77,6 +79,43 @@
                :delete-array-edge
                :delete-container
                :delete-value])
+
+(defmulti get-kws-in-schema c/schema->dispatch-type)
+
+(defmethod get-kws-in-schema :array
+  [schema]
+  (get-kws-in-schema (l/child-schema schema)))
+
+(defmethod get-kws-in-schema :map
+  [schema]
+  (get-kws-in-schema (l/child-schema schema)))
+
+(defmethod get-kws-in-schema :record
+  [schema]
+  (let [{:keys [fields]} (l/edn schema)]
+    (mapcat (fn [{field :name}]
+              (let [child-schema (l/child-schema schema field)]
+                (cons field (get-kws-in-schema child-schema))))
+            fields)))
+
+(defmethod get-kws-in-schema :union
+  [schema]
+  (mapcat get-kws-in-schema (l/member-schemas schema)))
+
+(defmethod get-kws-in-schema :single-value
+  [schema]
+  nil)
+
+(defn schema->kw-fns [schema]
+  (let [i->kw (-> (get-kws-in-schema schema)
+                  (set)
+                  (sort)
+                  (vec))
+        kw->i (reduce (fn [acc i]
+                        (assoc acc (i->kw i) i))
+                      {}
+                      (range (count i->kw)))]
+    (u/sym-map kw->i i->kw)))
 
 (defn insert-ba [ba index item]
   (let [len (count ba)
@@ -93,35 +132,57 @@
     new-ba))
 
 (defn write-path! [path ^MemorySegment mem-seg]
-  (let [path-len (count path)
-        last-i (dec path-len)]
-    (when (pos? path-len)
-      (loop [i 0]
-        (let [el (nth path i)
-              s (cond
-                  (keyword? el)
-                  (str keyword-marker el)
+  #_(let [path-len (count path)
+          last-i (dec path-len)]
+      (when (pos? path-len)
+        (loop [i 0]
+          (let [el (nth path i)
+                s (cond
+                    (keyword? el)
+                    (str keyword-marker el)
 
-                  (string? el)
-                  (str string-marker el)
+                    (string? el)
+                    (str string-marker el)
 
-                  :else
-                  (throw (ex-info
-                          (str "Bad path element `" (or el "nil") "`.")
-                          (u/sym-map path el))))]
-          (.set mem-seg
-                ValueLayout/JAVA_LONG
-                (long (* i path-element-len))
-                (long (lfp/fingerprint64 s)))
-          (when (< i last-i)
-            (recur (inc i)))))
-      (.set mem-seg
-            ValueLayout/JAVA_BYTE
-            (long path-len-offset)
-            (byte path-len)))))
+                    :else
+                    (throw (ex-info
+                            (str "Bad path element `" (or el "nil") "`.")
+                            (u/sym-map path el))))]
+            (.set mem-seg
+                  ValueLayout/JAVA_LONG
+                  (long (* i path-element-len))
+                  )
+            (when (< i last-i)
+              (recur (inc i)))))
+        (.set mem-seg xs!
+              ValueLayout/JAVA_BYTE
+              (long path-len-offset)
+              (byte path-len)))))
 
-(defn op-map->op-byte-array [op-map]
-  (let [ba (byte-array op-byte-array-len)
-        mem-seg ^MemorySegment (MemorySegment/ofArray ^bytes ba)]
-    (write-path! (vec (:path op-map)) mem-seg)
-    ba))
+(defn ->op-byte-array [{:keys [path schema kw->i]}]
+  #_(let [ba (byte-array op-byte-array-len)
+          mem-seg ^MemorySegment (MemorySegment/ofArray ^bytes ba)
+          path-v (vec path)]
+      (reduce
+       (fn [offset path-i]
+         (let [el (nth path-v path-i)
+               [l v] (cond
+                       (keyword? el)
+                       (let [kw-i (kw->i el)
+                             num-bytes (cond <)
+                             l (unchecked-byte
+                                (bit-or (unchecked-byte 2r10000000)
+                                        ))])
+                       (str keyword-marker el)
+
+                       (string? el)
+                       (str string-marker el)
+
+                       :else
+                       (throw (ex-info
+                               (str "Bad path element `" (or el "nil") "`.")
+                               (u/sym-map path el))))]
+           (if)))
+       32
+       (range (count path-v)))
+      ba))
